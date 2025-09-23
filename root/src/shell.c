5e7af98475f8763fc100ac5f99ee100170b610fe
Brendan Womer <bwbrendan@gmail.com>
2025-09-23 14:15:36 -0400

    inside main:     - added grab input from user     - free space used for grabbed input added function to handle pt2,3 expandTokens(tokenlist*):  - will expand variables tokens (starting with $)  - will expand tilde symbol used in directories

--- root/src/shell.c
+++ root/src/shell.c
@@ -3,18 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool expandTokens(tokenlist* tokens);
+
 
 int main(int argc, char** argv)
 {
-    while (1)
+    int count = 0;
+
+    while (count < 5)
     {
+        // char* temphostname;
         // print the shell's "prompt" using getenv() (format: USER@MACHINE:PWD>)
-        printf("%s@%s:%s>", getenv("$USER"), getenv("$MACHINE"), getenv("$PWD") )
+        printf("%s@%s:%s> \n", getenv("USER"), getenv("HOSTNAME"), getenv("PWD") );
+        tokenlist* input = get_tokens(get_input());
         // read and user's input
+
         // execute commands
 
-
+        free_tokens(input);
+        count++;
     }
-
+    exit(0);*
 
 }
+
+
+/*
+expandTokens(tokenlist&)
+*****
+passing in a tokenlist(tokenization of the user input) will handle parts 2 and 3
+- will expand token variable's symbolic representation ($USER)->(<username>), ($HOME)->(/home/username), ($SHELL)->(/bin/bash)
+- will expand token tilde's symbolic representation (~)->(/home/username), (~/path/of/folders)->(/home/username/path/of/folders)
+*/
+bool expandTokens(tokenlist* tokens)
+{
+    // expand variables
+
+
+    // expand tilde
+
+    
+    // return T/F
+}
+
+
