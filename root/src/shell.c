adf8ed94fa289c579a47a74f11b280615cc65cca
Brendan Womer <bwbrendan@gmail.com>
2025-09-24 18:10:44 -0400

    fixed part 2,3 to update tokenlists. started part 4 findPath(char* command):     - first, check to see if the command is already a path--wont need to be expanded     - check to see if $PATH exists before searching     - create a copy of $PATH and a temp str directory to store each deliminated element of $PATH

--- root/src/shell.c
+++ root/src/shell.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-char* expandToken(const char* token);
-// char* findPath(tokenlist* tokens);
+void expandTokens(tokenlist* tokens);
+char* findPath(char* tokens);
 void printTokenlist(tokenlist* tokens);
 
 
@@ -19,7 +20,7 @@ int main(int argc, char** argv)
         // print the shell's "prompt" using getenv() (format: USER@MACHINE:PWD>)
         printf("%s@%s:%s> \n", getenv("USER"), getenv("HOSTNAME"), getenv("PWD") );
         tokenlist* input = get_tokens(get_input());
-
+        expandTokens(input);
         // read and user's input
         printTokenlist(input);
 
@@ -41,7 +42,7 @@ void printTokenlist(tokenlist* tokens)
     {
         for (int i = 0; i < tokens->size; i++)
         {
-            printf("--%i: %s\n",i, expandToken(tokens->items[i]));
+            printf("--%i: %s\n",i, tokens->items[i]);
         }
     }
     else 
@@ -57,64 +58,50 @@ passing in a tokenlist(tokenization of the user input) will handle parts 2 and 3
 - will expand token tilde's symbolic representation (~)->(/home/username), (~/path/of/folders)->(/home/username/path/of/folders)
 - NOTE: PROJECT DOES NOT SAY HOW TO HANDLE EXPANDING VARIABLES THAT DON'T EXIST. NULL IS RETURNED, BUT IM NOT ENTIRELY SURE IF THEY WANT AN ERROR OR TO JUST IGNORE THE NULL RETURN AND CONTINUE RUNNING--DESPITE MOST LIKELY BREAKING THE FUNCTIONALITY.
 */
-char* expandToken(const char* inputToken)
+void expandTokens(tokenlist* tokens)
 {
-    size_t inputTokenLen = strlen(inputToken);
-    char* token = (char*)malloc(inputTokenLen+1);
-    strcpy(token, inputToken);
-    char* result = NULL;    // will be the expanded string that is returned
-    char* homePath = getenv("HOME");      // temp str to store the current home path ('~' expanded)
-    // printf("EXPANDTOKEN: token: %s\nstrlen(token):%ld\ninputTokenLen:%ld\n\n", token, strlen(token), inputTokenLen);
+    // iteratively expand variables 
+    for (int i = 0; i < tokens->size ; i++)
+    {    
+        // add a temporary variable to store the current token
+        char* currentToken = tokens->items[i];
+        char* expandedToken = NULL;
 
-    // token validation at start if the token passed is NULL
-    if (token == NULL)
-    {
-        printf("ERROR expandToken(const char*): token argument is NULL\n");
-        return NULL;
-    }
-
-    // case of Variable found
-    if (token[0] == '$') 
-    {
-        const char* varName = token+1;            // point to the value inside the string starting AFTER the '$'
-        const char* variableValue = getenv(varName);     // the expanded value of varName
-        size_t varValueLen = strlen(variableValue) +1;
-        // printf("\n----EXPANDTOKEN:\n token: %s \n variableValue: %s\n-------------------\n\n", token, variableValue);
-        
-        // if a valid expansion is found...
-        if (variableValue != NULL)
+        // caase of '$' is found at the beginning of the token
+        if (currentToken[0] == '$') 
         {
-            // realloc the size of result, who's size accounts for the difference in size accounting for the '$'.. (expanding with getenv() will make it smaller or larger)
-            result = (char*)realloc(result, varValueLen);
-            strcpy(result, variableValue);
-            // printf("\n----EXPANDTOKEN:\n token: %s \n variableValue: %s\n result: %s\n-----------------\n\n", token, variableValue, result);
-            free(token);
-            return result;
+            // varValue will store the expanded variable
+            char* varValue = getenv(currentToken+1);
+
+            if (varValue != NULL)
+            {
+                expandedToken = (char*)malloc(strlen(varValue) + 1);
+                strcpy(expandedToken, varValue);     // replace variable token with it's expanded form
+            }
+        }
+
+        // if a '~' is found and matches the requirements...
+        else if ( currentToken[0] == '~' && ( currentToken[1] == '/' || currentToken[1] == '\0' ) )
+        {
+            // expand the value of ~
+            const char* homePath = getenv("HOME");
+            if (homePath != NULL)
+            {
+                expandedToken = (char*)malloc(strlen(homePath) + strlen(currentToken+1) + 1);
+                // concatenate the homepath with the rest of the path           
+                strcpy(expandedToken, homePath);                            
+                strcat(expandedToken, (currentToken)+1);     // concatenate the remaining original string excluding the '~' element
+            }
+        } 
+
+        // update the tokenlist
+        if (expandedToken != NULL)
+        {
+            // printf("ERROR in expandTokens(): i=%d, tokens->items[i]=%s, strlen()=%d", i, (expandedToken), strlen(expandedToken) );
+            free(tokens->items[i]);
+            tokens->items[i] = expandedToken;
         }
     }
-    // case of tilde used symbolically
-    if ( token[0] == '~' && ( token[1] == '/' || token[1] == '\n' ) )
-    {
-        // error to handle issues with getenv() or missing env variables
-        if (homePath == NULL)
-        {
-            printf("ERROR expandToken(const char*): homePath is NULL\n");
-            return result;
-        }
-        // realloc result to fit the original token (excluding the ~) and the expanded homePath + 1 for delimiter
-        result = (char*)realloc(result, ( strlen(homePath)+strlen(token+1)+1 ) );
-        // construct the final result
-        strcpy(result, homePath);  
-        strcat(result, token+1);
-        free(token);
-        return result;
-
-    }
-    // case of command token that needs to be expanded.
-
-    // case other..
-    
-    return NULL;
 }
 
 /* char* findPath(char* token)
@@ -128,35 +115,34 @@ accepts a token and returns an expanded path for the command (ls -> /usr/bin/ls)
 
 */
 
-// char* findPath(char* token)
-// {
-//     // char* tempToken = NULL;
-//     // - check for slashes at start of token './' or '/'
-//     if (token[0] == '/' || ( token[0] == '.' && token[1] == '/' ) )
-//     {
+char* findPath(char* command)
+{
+    // case of path to command given-- check for slashes at start of token './' or '/'
+    if (command[0] == '/' || ( command[0] == '.' && command[1] == '/' ) )
+    {
+        if (access(command, X_OK) == 0)
+            return strdup(command);
+        return NULL;
+    }
 
-//     }
-//     // ... if it doesn't, don't search, just verify the path is valid
-//     char* expandedPath = getenv(token);
-//     if (expandedPath == NULL)
-//     {
-//         printf("ERROR in findPath(char*): non-path found, but returned NULL in getenv()\n");
-//         return NULL;
-//     }
-    
-//     // - if no slash and its not valid as-is, search $PATH
-//     else
-//     {
-//     // - tokenize the $PATH using strtok() using : as a delimiter
-    
-//     // - identify the token that matches corresponds with the command
-        
-//     }
-    
-//     // - check the final path 
+    // case of env variable given-- search $PATH
+    char* pathValue = getenv("PATH");
+    // if no $PATH env var there is no PATH to search-- return NULL
+    if (pathValue == NULL)
+    {
+        return NULL;
+    }
 
-//     return token;
-// }
+    // create a copy of $PATH
+    char* pathCopy = strdup(pathValue);
+    char* directory = strtok(pathCopy, ":");
+    
+    // - tokenize the $PATH using strtok() using : as a delimiter
+    // - identify the token that matches corresponds with the command
+    
+    // - check the final path 
+    return command;
+}
 
 
 
